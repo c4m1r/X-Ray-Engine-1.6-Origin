66c561170ac0fa8e300d43eee8b5d67261f50186
range_vs <lagosmod@gmail.com>
2026-03-29 00:51:32 +0300

    Remove old dependents

--- code/build_scripts/copy_lib_x64_debug.cmd
+++ code/build_scripts/copy_lib_x64_debug.cmd
@@ -1,10 +1,10 @@
-set pathToMkexp="C:\Program Files (x86)\Embarcadero\Studio\21.0\bin\mkexp.exe"
+set pathToMkexp="C:\Program Files (x86)\Embarcadero\Studio\37.0\bin\mkexp.exe"
 rem path to mkexp
-set pathToDlls=bins\Win64\Debug\
+set pathToDlls=..\..\bins\Win64\Debug\
 rem path to compiled dlls
-set pathToAddDlls=code\SDK\binaries\Win64\
+set pathToAddDlls=..\..\code\SDK\binaries\Win64\
 rem path to additional dlls
-set pathOutLibs=libraries\Win64\Debug\
+set pathOutLibs=..\..\libraries\Win64\Debug\
 rem path to out generated libraries
 
 %pathToMkexp% %pathOutLibs%etoolsb.a %pathToDlls%etools.dll
@@ -19,19 +19,21 @@ rem path to out generated libraries
 %pathToMkexp% %pathOutLibs%d3dx9d_41.a %pathToAddDlls%d3dx9d_41.dll"
 %pathToMkexp% %pathOutLibs%LWOB.a %pathToDlls%LWO.dll"
 
-copy code\SDK\binaries\Win64\FreeImage.dll %pathToDlls%FreeImage.dll
-copy code\SDK\binaries\Win64\amd_ags_x64.dll %pathToDlls%amd_ags_x64.dll
-copy code\SDK\binaries\Win64\D3DX9d_41.dll %pathToDlls%D3DX9d_41.dll
-copy code\SDK\binaries\Win64\bcbsmp270.bpl %pathToDlls%bcbsmp270.bpl
-copy code\SDK\binaries\Win64\borlndmm.dll %pathToDlls%borlndmm.dll
-copy code\SDK\binaries\Win64\cc64270.dll %pathToDlls%cc64270.dll
-copy code\SDK\binaries\Win64\cc64270mt.dll %pathToDlls%cc64270mt.dll
-copy code\SDK\binaries\Win64\rtl270.bpl %pathToDlls%rtl270.bpl
-copy code\SDK\binaries\Win64\vcl270.bpl %pathToDlls%vcl270.bpl
-copy code\SDK\binaries\Win64\vclimg270.bpl %pathToDlls%vclimg270.bpl
-copy code\SDK\binaries\Win64\vclx270.bpl %pathToDlls%vclx270.bpl
-copy code\SDK\binaries\Win64\msvcp140d.dll %pathToDlls%msvcp140d.dll
-copy code\SDK\binaries\Win64\vcruntime140d.dll %pathToDlls%vcruntime140d.dll
-copy code\SDK\binaries\Win64\vcruntime140_1d.dll %pathToDlls%vcruntime140_1d.dll
+copy ..\..\code\SDK\binaries\Win64\FreeImage.dll %pathToDlls%FreeImage.dll
+copy ..\..\code\SDK\binaries\Win64\amd_ags_x64.dll %pathToDlls%amd_ags_x64.dll
+copy ..\..\code\SDK\binaries\Win64\D3DX9d_41.dll %pathToDlls%D3DX9d_41.dll
+
+copy ..\..\code\SDK\binaries\Win64\bcbsmp270.bpl %pathToDlls%bcbsmp270.bpl
+copy ..\..\code\SDK\binaries\Win64\borlndmm.dll %pathToDlls%borlndmm.dll
+copy ..\..\code\SDK\binaries\Win64\cc64270.dll %pathToDlls%cc64270.dll
+copy ..\..\code\SDK\binaries\Win64\cc64270mt.dll %pathToDlls%cc64270mt.dll
+copy ..\..\code\SDK\binaries\Win64\rtl270.bpl %pathToDlls%rtl270.bpl
+copy ..\..\code\SDK\binaries\Win64\vcl270.bpl %pathToDlls%vcl270.bpl
+copy ..\..\code\SDK\binaries\Win64\vclimg270.bpl %pathToDlls%vclimg270.bpl
+copy ..\..\code\SDK\binaries\Win64\vclx270.bpl %pathToDlls%vclx270.bpl
+
+copy ..\..\code\SDK\binaries\Win64\msvcp140d.dll %pathToDlls%msvcp140d.dll
+copy ..\..\code\SDK\binaries\Win64\vcruntime140d.dll %pathToDlls%vcruntime140d.dll
+copy ..\..\code\SDK\binaries\Win64\vcruntime140_1d.dll %pathToDlls%vcruntime140_1d.dll
 
 pause
--- code/build_scripts/copy_lib_x64_release.cmd
+++ code/build_scripts/copy_lib_x64_release.cmd
@@ -1,4 +1,4 @@
-set pathToMkexp="C:\Program Files (x86)\Embarcadero\Studio\21.0\bin\mkexp.exe"
+set pathToMkexp="C:\Program Files (x86)\Embarcadero\Studio\37.0\bin\mkexp.exe"
 rem path to coff2omf
 set pathToDlls=bins\Win64\Release\
 rem path to compiled dlls
@@ -21,6 +21,7 @@ rem path to out generated libraries
 
 copy code\SDK\binaries\Win64\FreeImage.dll %pathToDlls%FreeImage.dll
 copy code\SDK\binaries\Win64\amd_ags_x64.dll %pathToDlls%amd_ags_x64.dll
+
 copy code\SDK\binaries\Win64\bcbsmp270.bpl %pathToDlls%bcbsmp270.bpl
 copy code\SDK\binaries\Win64\borlndmm.dll %pathToDlls%borlndmm.dll
 copy code\SDK\binaries\Win64\cc64270.dll %pathToDlls%cc64270.dll
