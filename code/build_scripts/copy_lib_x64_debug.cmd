d636072967ef16beaf5d04a0a03db5459e682f3e
range_vs <lagosmod@gmail.com>
2026-03-30 01:15:00 +0300

    Fix todo & bat

--- code/build_scripts/copy_lib_x64_debug.cmd
+++ code/build_scripts/copy_lib_x64_debug.cmd
@@ -1,10 +1,5 @@
-set pathToMkexp="C:\Program Files (x86)\Embarcadero\Studio\37.0\bin\mkexp.exe"
-rem path to mkexp
 set pathToDlls=..\..\bins\Win64\Debug\
 rem path to compiled dlls
-set pathToAddDlls=..\..\code\SDK\binaries\Win64\
-rem path to additional dlls
-set pathOutLibs=..\..\libraries\Win64\Debug\
 
 copy ..\..\code\SDK\binaries\Win64\FreeImage.dll %pathToDlls%FreeImage.dll
 copy ..\..\code\SDK\binaries\Win64\amd_ags_x64.dll %pathToDlls%amd_ags_x64.dll
--- code/build_scripts/copy_lib_x64_release.cmd
+++ code/build_scripts/copy_lib_x64_release.cmd
@@ -1,23 +1,8 @@
-set pathToMkexp="C:\Program Files (x86)\Embarcadero\Studio\37.0\bin\mkexp.exe"
-rem path to coff2omf
-set pathToDlls=bins\Win64\Release\
+set pathToDlls=..\..\bins\Win64\Debug\
 rem path to compiled dlls
-set pathToAddDlls=code\SDK\binaries\Win64\
-rem path to additional dlls
-set pathOutLibs=libraries\Win64\Release\
-rem path to out generated libraries
 
-%pathToMkexp% %pathOutLibs%d3dx9_41.lib %pathToAddDlls%D3DX9_41.dll
-copy code\SDK\binaries\Win64\FreeImage.dll %pathToDlls%FreeImage.dll
-copy code\SDK\binaries\Win64\amd_ags_x64.dll %pathToDlls%amd_ags_x64.dll
+copy ..\..\code\SDK\binaries\Win64\FreeImage.dll %pathToDlls%FreeImage.dll
+copy ..\..\code\SDK\binaries\Win64\amd_ags_x64.dll %pathToDlls%amd_ags_x64.dll
 
-copy code\SDK\binaries\Win64\bcbsmp270.bpl %pathToDlls%bcbsmp270.bpl
-copy code\SDK\binaries\Win64\borlndmm.dll %pathToDlls%borlndmm.dll
-copy code\SDK\binaries\Win64\cc64270.dll %pathToDlls%cc64270.dll
-copy code\SDK\binaries\Win64\cc64270mt.dll %pathToDlls%cc64270mt.dll
-copy code\SDK\binaries\Win64\rtl270.bpl %pathToDlls%rtl270.bpl
-copy code\SDK\binaries\Win64\vcl270.bpl %pathToDlls%vcl270.bpl
-copy code\SDK\binaries\Win64\vclimg270.bpl %pathToDlls%vclimg270.bpl
-copy code\SDK\binaries\Win64\vclx270.bpl %pathToDlls%vclx270.bpl
 
 pause
