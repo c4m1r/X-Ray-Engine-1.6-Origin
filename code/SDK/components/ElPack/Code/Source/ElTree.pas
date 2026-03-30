c5ebf71f4bb84a337e432c5d9ec44d88e982e9df
range_vs <lagosmod@gmail.com>
2026-03-31 01:02:18 +0300

    Fix old pascal bug

--- code/SDK/components/ElPack/Code/Source/ElTree.pas
+++ code/SDK/components/ElPack/Code/Source/ElTree.pas
@@ -26140,7 +26140,7 @@ function TCustomElTree.SetScrollInfo(hWnd: HWND; BarFlag: Integer; const ScrollI
 begin
   if (hWnd = FHScrollBar.Handle) or (hWnd = FVScrollBar.Handle) then
   begin
-    result := SendMessage(hWnd, SBM_SetScrollInfo, Integer(Redraw), Integer(@ScrollInfo));
+    result := SendMessage(hWnd, SBM_SetScrollInfo, WPARAM(Redraw), LPARAM(@ScrollInfo));
   end else result := -1;
 end;
 
@@ -26148,7 +26148,7 @@ function TCustomElTree.GetScrollInfo(hWnd: HWND; BarFlag: Integer; var ScrollInf
 begin
   if (hWnd = FHScrollBar.Handle) or (hWnd = FVScrollBar.Handle) then
   begin
-    SendMessage(hWnd, SBM_GetScrollInfo, 0, Integer(@ScrollInfo));
+    SendMessage(hWnd, SBM_GetScrollInfo, 0, LPARAM(@ScrollInfo));
     result := BOOL(true);
   end else result := BOOL(false);
 end;
