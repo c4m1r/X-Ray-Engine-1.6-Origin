22c50c5d96b2e26257dd5356979f7ca894c252e1
range_vs <lagosmod@gmail.com>
2026-04-04 18:39:10 +0300

    New build scripts

---
+++ build-scripts/copy_lib_x64_debug.cmd
@@ -0,0 +1,14 @@
+@echo off
+setlocal
+
+set "ROOT=%~dp0.."
+for %%I in ("%ROOT%") do set "ROOT=%%~fI"
+
+set "pathToDlls=%ROOT%\bins\Win64\Debug"
+rem path to compiled dlls
+
+copy /Y "%ROOT%\code\SDK\binaries\Win64\FreeImage.dll" "%pathToDlls%\FreeImage.dll"
+copy /Y "%ROOT%\code\SDK\binaries\Win64\amd_ags_x64.dll" "%pathToDlls%\amd_ags_x64.dll"
+copy /Y "%ROOT%\code\SDK\binaries\Win64\D3DX9d_41.dll" "%pathToDlls%\D3DX9d_41.dll"
+
+if /I not "%~1"=="--no-pause" pause
--- code/build_scripts/copy_lib_x64_debug.cmd
+++
@@ -1,9 +0,0 @@
-set pathToDlls=..\..\bins\Win64\Debug\
-rem path to compiled dlls
-
-copy ..\..\code\SDK\binaries\Win64\FreeImage.dll %pathToDlls%FreeImage.dll
-copy ..\..\code\SDK\binaries\Win64\amd_ags_x64.dll %pathToDlls%amd_ags_x64.dll
-copy ..\..\code\SDK\binaries\Win64\D3DX9d_41.dll %pathToDlls%D3DX9d_41.dll
-copy ..\..\code\SDK\binaries\Win64\D3DX9d_41.dll %pathToDlls%D3DX9_41.dll
-
-pause
